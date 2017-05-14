5b40f1a60633bf7848feee5b0f7adfdedf32a0de
Julio <julio.garcia@galavantier.com>
2017-05-13 18:46:43 -0700

    Creating post call for create a new message

--- endpoints/message/create.js
+++ endpoints/message/create.js
@@ -4,32 +4,24 @@
  const AWS = require('aws-sdk');
  
  const dynamoDb = new AWS.DynamoDB.DocumentClient();
- const TableName = 'message-'+process.env.custom_stage;
  
  module.exports.create = (event, context, callback) => {
    const timestamp = new Date().getTime();
    const data = JSON.parse(event.body);
-   const response = {
-       statusCode: 200,
-       body: JSON.stringify(event),
-    };
-   callback(null,response);
-   return;
    if (typeof data.text !== 'string') {
      console.error('Validation Failed'); // eslint-disable-line no-console
      callback(new Error('Couldn\'t create the todo item.'));
      return;
    }
- 
+ callback(null,data);
    const params = {
-     TableName: TableName,
+     TableName: 'users',
      Item: {
        id: uuid.v1(),
-       date: timestamp,
-       type: type,
-       sender: sender,
-       receiver: receiver,
-       html: html,
+       text: data.text,
+       checked: false,
+       createdAt: timestamp,
+       updatedAt: timestamp,
      },
    };
  
@@ -42,6 +34,11 @@
        return;
      }
  
-
+     // create a response
+     const response = {
+       statusCode: 200,
+       body: JSON.stringify(result.Item),
+     };
+     callback(null, response);
    });
  };
