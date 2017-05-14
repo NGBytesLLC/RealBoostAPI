ba6b55ac2987298f6a483f0d650646ba640bde8b
Julio <julio.garcia@galavantier.com>
2017-05-13 17:05:12 -0700

    Creating the first response for return messages

--- endpoints/message/list.js
+++ endpoints/message/list.js
@@ -18,11 +18,15 @@ module.exports.list = (event, context, callback) => {
       callback(new Error('Couldn\'t fetch the users messages.'));
       return;
     }
-
+    const data = {
+    	data: JSON.stringify(result.Items),
+    	message: "",
+    	success: "true"
+    },
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Items),
+      body: data,
     };
     callback(null, response);
   });
